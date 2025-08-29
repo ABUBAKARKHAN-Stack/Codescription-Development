26641eb40d79896e4348c87b2e79cb0eb8e3c186
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-08-30 00:48:46 +0500

    fix next config file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [{ protocol: "https", hostname: "www.google.com" }],
-  },
-
-  experimental: {
-    optimizeCss: true,
   },
 };
 
